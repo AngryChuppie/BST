2f7c3e6b78bc209cb26c12e57962331358390531
meloj <meloj@DESKTOP-I44JMIE.Home>
2017-02-14 20:03:25 +0000

    partsRecorded into github

---
+++ PartsRec/src/partsrec/menu.java
@@ -0,0 +1,18 @@
+package sample;/**
+ * Created by meloj on 08/02/2017.
+ */
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class menu extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
